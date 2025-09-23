f6d2b7ea56703b96d666efa962aadb8684f4e252
vsatheesh <viswanathan.satheesh@gmail.com>
2025-09-23 01:04:45 -0500

    edits to 04_fastqc_parallel.sh

--- 00_scripts/04_fastqc_parallel.sh
+++ 00_scripts/04_fastqc_parallel.sh
@@ -2,8 +2,8 @@ module purge
 module load parallel
 module load fastqc 
 
-mkdir -p 02d_fastqc_parallel
-# samplename=$(basename "$file" .fastq.gz)
+mkdir -p 05_fastqc_parallel
+
 parallel -j10 \
-  'fastqc {1} -o 02d_fastqc_parallel/ > logs/02_fastqc_parallel_{1/.}.log 2>&1' \
+  'fastqc {1} -o 05_fastqc_parallel/ > logs/04_fastqc_parallel_{1/.}.log 2>&1' \
   ::: 01_data/*.fastq.gz
